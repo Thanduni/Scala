3c096274edf01f19c4b2f3553ac9323f36d7f5ec
Thanduni <76160782+Thanduni@users.noreply.github.com>
2022-07-10 17:33:16 +0530

    Update question3.scala

--- question3.scala
+++ question3.scala
@@ -6,19 +6,26 @@ object question3 extends App{
     var d=5
     var k =4.3f
     var g = 4.0f
-
+    
+    //values in Scala can not be incremented or decremented
+    
+    //println(--b * a + c * d--)   
     b-=1
     println(b*a+c*d)
     d-=1
-
+    
+    //println(a++)
     println(a)
     a+=1
-
+    
+    //println(-2 * (g - k) + c)
     println(-2 * (g - k) + c)
-
+    
+    //println(c == c++)
     println(c==c)
     c+=1
-
+    
+    //println(c == ++c * a++)
     c+=1
     println(c==c*a)
     a+=1
